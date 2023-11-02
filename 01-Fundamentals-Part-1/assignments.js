0c0f12be94e1848f64f5efaccb3998baaed01ffa
Ilia <ilia.stepanov07@gmail.com>
2023-11-02 11:37:22 +0400

    fixed Data types task

--- 01-Fundamentals-Part-1/assignments.js
+++ 01-Fundamentals-Part-1/assignments.js
@@ -21,17 +21,17 @@ console.log(population);
 // 1. Declare a variable called 'isIsland' and set its value according to your country. 
 // The variable should hold a Boolean value. Also declare a variable 'language', 
 // but don't assign it any value yet
-// 2. Logthetypesof'isIsland','population','country'and'language' to the console
+// 2. Log the types of'isIsland','population','country'and'language' to the console
 
 let isIsland;
 let language;
 
 isIsland = false;
 
-console.log(isIsland);
-console.log(population);
-console.log(country);
-console.log(language);
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
 
 
 // LECTURE: let, const and var
