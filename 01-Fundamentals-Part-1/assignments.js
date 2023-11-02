ce585412d370839bed4c808c6c8267ae5853651e
Ilia Stepanov <ilia.stepanov07@gmail.com>
2023-11-02 23:35:54 +0400

    added tasks on functions

--- 01-Fundamentals-Part-1/assignments.js
+++ 01-Fundamentals-Part-1/assignments.js
@@ -260,7 +260,12 @@ console.log(`${country}'s population is ${population > 33 ? "above" : "below"} a
 
 
 
-// Functions - ADD DETAILS!
+// LECTURE: Functions
+// 1. Write a function called 'describeCountry' which takes three parameters: 'country', 
+// 'population', and 'capitalCity'. Based on this input, the function returns a string with 
+// this format: 'Finland has 6 million people and its capital city is Helsinki.'
+// 2. Call this function 3 times, with input data for 3 different countries. 
+// Store the returned values in 3 different variables, and log them to the console.
 
 function describeCountry(country, population, capitalCity) {
     return `${country} has ${population} million people and its capital city is ${capitalCity}`
@@ -269,3 +274,43 @@ function describeCountry(country, population, capitalCity) {
 countryProfile1 = describeCountry('UAE', 10, 'Abu Dhabi');
 countryProfile2 = describeCountry('Russia', 145, 'Moscow');
 countryProfile3 = describeCountry('Finland', 6, 'Helsinki');
+
+
+// LECTURE: Function Declarations vs. Expressions
+// 1. The world population is 7900 million people. Create a function declaration called 
+// 'percentageOfWorld1' which receives a 'population' value, and returns the percentage 
+// of the world population that the given population represents. For example, 
+// China has 1441 million people, so it's about 18.2% of the world population.
+// 2. To calculate the percentage, divide the given 'population' value by 7900 and then multiply by 100.
+// 3. Call 'percentageOfWorld1' for 3 populations of countries of your choice, 
+// store the results into variables, and log them to the console.
+// 4. Create a function expression which does the exact same thing, called 'percentageOfWorld2', 
+// and also call it with 3 country populations (can be the same populations).
+
+function percentageOfWorld1(population) {
+    let worldPopulation = 7900;
+    return population / worldPopulation * 100;
+}
+
+let percentageOfWorld2 = function(population) {
+    let worldPopulation = 7900;
+    return population / worldPopulation * 100;
+}
+
+
+// LECTURE: Arrow Functions
+// 1. Recreate the last assignment, but this time create an arrow function called 'percentageOfWorld3'
+
+let worldPopulation = 7900;
+let percentageOfWorld3 = (population, worldPopulation) => population / worldPopulation * 100;
+
+
+
+
+
+
+
+
+
+
+
