0a6784310f478b5cf2dbbb68c9337d4df9180373
Ilia Stepanov <ilia.stepanov07@gmail.com>
2023-11-12 00:39:32 +0400

    added keypress in guess the number project

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -1,6 +1,6 @@
-"use strict";
+'use strict';
 
-document.querySelector(".score").textContent = 10;
+document.querySelector('.score').textContent = 10;
 
 let score = 10;
 
@@ -8,60 +8,69 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 let highscore = 0;
 
-document.querySelector(".check").addEventListener("click", function () {
-  const guess = Number(document.querySelector(".guess").value);
+const guessInput = document.querySelector('.guess');
 
+function checkGuess() {
+  const guess = Number(guessInput.value);
   if (!guess) {
-    setMessage("No number");
+    setMessage('No number');
   } else if (score > 0) {
     if (guess === secretNumber) {
       setNumber(secretNumber);
-      setMessage("Correct Number!");
-      setBackgroundColor("#60b347");
-      setNumberWidth("30rem");
+      setMessage('Correct Number!');
+      setBackgroundColor('#60b347');
+      setNumberWidth('30rem');
 
       if (score > highscore) {
-        document.querySelector(".highscore").textContent = score;
+        document.querySelector('.highscore').textContent = score;
         highscore = score;
       }
     } else if (guess !== secretNumber) {
-      setMessage(guess > secretNumber ? "Too High!" : "Too Low!");
+      setMessage(guess > secretNumber ? 'Too High!' : 'Too Low!');
       setScore(--score);
     }
   }
   if (score < 1) {
-    setMessage("You lost the game!");
-    setBackgroundColor("#e31616");
+    setMessage('You lost the game!');
+    setBackgroundColor('#e31616');
   }
-});
+}
 
-document.querySelector(".again").addEventListener("click", function () {
+document.querySelector('.check').addEventListener('click', checkGuess);
+
+document.querySelector('.again').addEventListener('click', function () {
   score = 10;
-  setMessage("Start guessing...");
+  setMessage('Start guessing...');
   setScore(score);
-  document.querySelector(".guess").value = "";
-  setBackgroundColor("#222");
-  setNumberWidth("15rem");
-  setNumber("?");
+  document.querySelector('.guess').value = '';
+  setBackgroundColor('#222');
+  setNumberWidth('15rem');
+  setNumber('?');
   secretNumber = Math.trunc(Math.random() * 20) + 1;
 });
 
 function setMessage(message) {
-  document.querySelector(".message").textContent = message;
+  document.querySelector('.message').textContent = message;
 }
 
 function setScore(score) {
-  document.querySelector(".score").textContent = score;
+  document.querySelector('.score').textContent = score;
 }
 
 function setBackgroundColor(color) {
-  document.querySelector("body").style.backgroundColor = color;
+  document.querySelector('body').style.backgroundColor = color;
 }
 
 function setNumberWidth(width) {
-  document.querySelector(".number").style.width = width;
+  document.querySelector('.number').style.width = width;
 }
 
 function setNumber(number) {
-  document.querySelector(".number").textContent = number;
+  document.querySelector('.number').textContent = number;
 }
+
+guessInput.addEventListener('keypress', function (event) {
+  if (event.key === 'Enter') {
+    checkGuess();
+  }
+});
