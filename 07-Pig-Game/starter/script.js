39b5b54b7aa29f27eb7461e5499b1f0626356ec8
Ilia Stepanov <ilia.stepanov07@gmail.com>
2023-11-12 17:09:03 +0400

    added official version. basic setting 0 to vars

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const score0EL = document.querySelector('#score--0');
+const score1EL = document.getElementById('score--1');
+const dicePictureEl = document.querySelector('.dice');
+
+score0EL.textContent = 0;
+score1EL.textContent = 0;
+dicePictureEl.classList.add('hidden');
